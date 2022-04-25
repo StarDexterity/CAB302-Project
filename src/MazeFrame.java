9a65faec198a332a026a74fae2d93623bf1a6622
StarDexterity <72377348+StarDexterity@users.noreply.github.com>
2022-04-26 02:30:39 +1000

    created a placeholder for the maze

--- src/MazeFrame.java
+++ src/MazeFrame.java
@@ -5,42 +5,62 @@ import java.awt.event.ActionListener;
 
 public class MazeFrame extends JFrame {
     public static final int WIDTH = 1200;
-    public static final int HEIGHT = 600;
+    public static final int HEIGHT = 700;
 
     public static final int MIN_WIDTH = 1000;
     public static final int MIN_HEIGHT = 500;
 
-    private JPanel optionsPanel;
+    // may change in the future
+    public static final boolean RESIZEABLE = false;
+
+    private OptionsPanel optionsPanel;
 
     public MazeFrame(String s) {
         super(s);
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                | UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        }
 
         createGUI();
     }
 
     private void createGUI() {
-        setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
 
-        // panel related code will go here
         Container c = this.getContentPane();
+        optionsPanel = new OptionsPanel();
 
-        OptionsPanel optionsPanel = new OptionsPanel();
 
 
+        // layout code
         c.add(optionsPanel, BorderLayout.WEST);
+        c.add(placeHolder(), BorderLayout.CENTER);
+
+
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
+        setResizable(RESIZEABLE);
         setSize(WIDTH,HEIGHT);
+    }
+
+    /**
+     * Creates a placeholder panel for the center
+     * @return
+     */
+    private JPanel placeHolder() {
+        // create the placeholder panel, with GridBagLayout and an empty border
+        JPanel placeHolder = new JPanel();
+        placeHolder.setLayout(new GridBagLayout());
+        placeHolder.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+
+        // grid bag constraints
+        GridBagConstraints gbc = GridBagHelper.createDefaultGBC();
+
+        // create inner white panel
+        JPanel white = new JPanel();
+        white.setBackground(Color.WHITE);
+        GridBagHelper.addToPanel(placeHolder, white, gbc, 0, 0, 1, 1);
+        return placeHolder;
     }
 }
 
