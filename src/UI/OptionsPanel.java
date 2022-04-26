ad1cb762455adf9a88708bb0ed99b602018ffd3c
StarDexterity <72377348+StarDexterity@users.noreply.github.com>
2022-04-27 02:45:21 +1000

    moved ui classes to ui folder

--- src/UI/OptionsPanel.java
+++ src/UI/OptionsPanel.java
@@ -1,9 +1,8 @@
+package UI;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 public class OptionsPanel extends JPanel {
 
