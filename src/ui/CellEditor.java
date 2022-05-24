b1ec6b57d3ea04e4c445f465586368b2dc63564c
StarDexterity <72377348+StarDexterity@users.noreply.github.com>
2022-05-25 01:06:12 +1000

    cell editor prototype created

--- src/ui/CellEditor.java
+++ src/ui/CellEditor.java
@@ -6,8 +6,54 @@ import java.awt.*;
 
 public class CellEditor extends JPanel {
 
-    public CellEditor() {
-        Border innerBorder = BorderFactory.createTitledBorder("Display options");
+    EditPage editPage;
+    JLabel title;
+    CellDisplay cellDisplay;
+    JButton toggleRight;
+    JButton toggleLeft;
+    JButton toggleTop;
+    JButton toggleBottom;
+    JButton toggleAll;
+    JButton insertImage;
+
+
+    public CellEditor(EditPage editPage) {
+        this.editPage = editPage;
+
+        // create components
+        title = new JLabel("Cell editor [F3]");
+        title.setFont(new Font("Tahoma", Font.PLAIN,  18));
+        cellDisplay = new CellDisplay();
+
+        toggleTop = new JButton("Toggle top");
+        toggleTop.addActionListener(e -> {
+            boolean topWallEnabled = cellDisplay.isTopWallEnabled();
+            cellDisplay.setTopWallEnabled(!topWallEnabled);
+        });
+
+        toggleLeft = new JButton("Toggle left");
+        toggleLeft.addActionListener(e -> {
+            boolean leftWallEnabled = cellDisplay.isLeftWallEnabled();
+            cellDisplay.setLeftWallEnabled(!leftWallEnabled);
+        });
+
+        toggleBottom = new JButton("Toggle bottom");
+        toggleBottom.addActionListener(e -> {
+            boolean bottomWallEnabled = cellDisplay.isBottomWallEnabled();
+            cellDisplay.setBottomWallEnabled(!bottomWallEnabled);
+        });
+
+        toggleRight = new JButton("Toggle Right");
+        toggleRight.addActionListener(e -> {
+            boolean rightWallEnabled = cellDisplay.isRightWallEnabled();
+            cellDisplay.setRightWallEnabled(!rightWallEnabled);
+        });
+
+        toggleAll = new JButton("Toggle all");
+        insertImage = new JButton("Insert image");
+
+
+        Border innerBorder = BorderFactory.createTitledBorder("");
         Border outerBorder = BorderFactory.createEmptyBorder(5,10,5,10);
         setBorder(BorderFactory.createCompoundBorder(outerBorder,innerBorder));
 
@@ -18,7 +64,41 @@ public class CellEditor extends JPanel {
         setLayout(new GridBagLayout());
 
         GridBagConstraints gbc = GridBagHelper.createDefaultGBC();
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
 
-        //GridBagHelper.addToPanel(this, showGrid, gbc, 0, 1, 1, 1);
+        int y = 0;
+
+        // row 1
+        GridBagHelper.addToPanel(this, title, gbc, 0, y, 1, 1);
+
+        // row 2
+        y++;
+        GridBagHelper.addToPanel(this, cellDisplay, gbc, 0, y, 1, 1);
+
+        // row 3
+        y++;
+        GridBagHelper.addToPanel(this, toggleTop, gbc, 0, y, 1, 1);
+
+        // row 4
+        y++;
+        GridBagHelper.addToPanel(this, toggleLeft, gbc, 0, y, 1, 1);
+
+        // row 5
+        y++;
+        GridBagHelper.addToPanel(this, toggleBottom, gbc, 0, y, 1, 1);
+
+        // row 6
+        y++;
+        GridBagHelper.addToPanel(this, toggleRight, gbc, 0, y, 1, 1);
+
+        // row 7
+        y++;
+        GridBagHelper.addToPanel(this, toggleAll, gbc, 0, y, 1, 1);
+
+        // row 8
+        y++;
+        GridBagHelper.addToPanel(this, insertImage, gbc, 0, y, 1, 1);
+
     }
 }
